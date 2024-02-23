2cff2a0b9a9b59c773ccb6283bb0c55c1079a02e
Мария Терентьева <MTerentyeva@rpa-robin.ru>
2024-02-23 16:17:15 +0300

    fix entry-client

--- packages/client/src/entry-client.tsx
+++ packages/client/src/entry-client.tsx
@@ -2,8 +2,6 @@ import ReactDOM from 'react-dom/client'
 import { registerSW } from './utils/service-worker'
 import { App } from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-)
+ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />)
 
 registerSW('/net-or-cache-sw.js')
