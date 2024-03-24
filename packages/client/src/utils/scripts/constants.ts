85b02d8fcd7aeabd983d5cfe48702a23fd037df7
Evgeniia Briazgina <zmenka345@gmail.com>
2024-03-24 13:41:58 +0100

    ycloud

--- packages/client/src/utils/scripts/constants.ts
+++ packages/client/src/utils/scripts/constants.ts
@@ -5,7 +5,7 @@ import axios, {
 } from 'axios'
 
 export const API_URL = 'https://ya-praktikum.tech/api/v2'
-export const FORUM_API_URL = `http://localhost:3001/api/forum`
+
 export const getOauthUrl = (clientId: string, redirectUri: string) =>
   `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`
 
@@ -17,8 +17,7 @@ export const isServer = !(
 export const RATING_FIELD_NAME = 'ratingSlytherinTeam'
 export const TEAM_NAME = 'slytherin'
 
-export const SlytherinApiUrl =
-  process.env.SERVER_URL || 'http://localhost:3001/api'
+export const SlytherinApiUrl = '/api/forum'
 
 export const slytherinApi: AxiosInstance = axios.create({
   baseURL: SlytherinApiUrl,
